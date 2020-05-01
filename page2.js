838313f6d95a3a8dacac120c4792b5c7d37296af
Paul Baumgarten <pbaumgarten@gmail.com>
2020-05-01 22:13:04 +0800

    Cloud firestore example added

--- page2.js
+++ page2.js
@@ -12,6 +12,44 @@ function loggedIn() {
     document.querySelector("#welcome").innerHTML = "Welcome "+user.displayName;
     let img = document.querySelector("#profilePic");
     img.src = user.photoURL;
+    // Create our database (Firebase Cloud Firestore) object   
+    let db = firebase.firestore();
+    // Update the database using collection="profile" and documentid=user.email
+    db.collection("profile").doc(user.uid).get().then(function(doc) {
+        if (doc.exists) {
+            let record = doc.data();
+            console.log("loggedIn read document "+user.email+" as:", record);
+            document.querySelector("input[name='hometown']").value = record['hometown'];
+            document.querySelector("input[name='dinnerguest']").value = record['dinnerguest'];
+            document.querySelector("input[name='yeartobeborn']").value = record['yeartobeborn']
+            document.querySelector("textarea[name='bio']").value = record['bio'];
+        }
+    }).catch(function(error) {
+        console.log("loggedIn error reading document "+user.email+" as:", error);
+    });
+}
+
+function update() {
+    // Verify we are logged in
+    let user = firebase.auth().currentUser;
+    if (user) {
+        // Create our JSON object to store
+        let record = {};
+        record['name'] = user.displayName;
+        record['email'] = user.email;
+        record['hometown'] = document.querySelector("input[name='hometown']").value;
+        record['dinnerguest'] = document.querySelector("input[name='dinnerguest']").value;
+        record['yeartobeborn'] = document.querySelector("input[name='yeartobeborn']").value;
+        record['bio'] = document.querySelector("textarea[name='bio']").value;
+        // Create our database object
+        let db = firebase.firestore();
+        // Update the database using collection="profile" and documentid=user.email
+        db.collection("profile").doc(user.uid).set(record).then(function() {
+            console.log("update successful for "+user.email);
+        }).catch(function(error) {
+            console.log("update error for "+user.email, error);
+        });
+    }
 }
 
 function main() {
